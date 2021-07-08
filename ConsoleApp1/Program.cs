637d8a2994ee44d7357c47ab32337a7dc144d6de
Liew Jun Kang <ljk_jgs@hotmail.com>
2021-07-08 19:00:07 +0800

    Create Project
    Add mini tools project

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using MiniTools;
+using System;
+using System.Reflection;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = @"StatCode: 00
+                            StatName: captured
+                            TranID: 35540257
+                            Amount: 12.00
+                            Domain: Payment.com.my
+                            Channel: ALB-Paymex
+                            VrfKey: ded0adf6fcbfa6bcaccf465750f9f61a
+                            Currency: MYR
+                            ErrorCode: 
+                            ErrorDesc: ";
+
+
+
+            GatewayResult gatewayResult = PlainTextToClassObjectConverter.ConvertPlainTextToClass<GatewayResult>(input);
+
+            Console.WriteLine("Finsih");
+        }
+    }
+
+    public class GatewayResult
+    {
+        public string StatCode { get; set; }
+        public string StatName { get; set; }
+        public string TranID { get; set; }
+        public decimal Amount { get; set; }
+        public string Domain { get; set; }
+        public string Channel { get; set; }
+        public string VrfKey { get; set; }
+        public string Currency { get; set; }
+        public string ErrorCode { get; set; }
+        public string ErrorDesc { get; set; }
+    }
+}
---
+++ MiniTools/PlainTextToClassObjectConverter.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace MiniTools
+{
+    /// <summary>
+    ///    Convert a plain text instead of Json/XML result into a class object
+    /// </summary>    
+    public static class PlainTextToClassObjectConverter
+    {
+        /*
+         * propertyDelimiter = a split delimiter for split different property inside plain text
+         * propertyValueDelimiter = a split delimiter for split property and value for each set of property array.
+         */
+        public static T ConvertPlainTextToClass<T>(string text, string propertyDelimiter = "\r\n", string propertyValueDelimiter = ":")
+        {
+            var type = typeof(T);
+            T instance = (T)Activator.CreateInstance(type);
+
+            string[] inputList = text.Split(propertyDelimiter);
+
+            foreach (var inputValue in inputList)
+            {
+                string[] classProperty = inputValue.Split(propertyValueDelimiter);
+                var property = type.GetProperty(classProperty[0].Trim());
+                string propertyValue = classProperty[1].Trim();
+
+                try
+                {
+                    switch (property.PropertyType.Name.ToLower())
+                    {
+                        case "double":
+                            property.SetValue(instance, double.Parse(propertyValue));
+                            break;
+                        case "decimal":
+                            property.SetValue(instance, decimal.Parse(propertyValue));
+                            break;
+                        case "float":
+                            property.SetValue(instance, float.Parse(propertyValue));
+                            break;
+                        case "int":
+                            property.SetValue(instance, int.Parse(propertyValue));
+                            break;
+                        case "string":
+                            property.SetValue(instance, propertyValue);
+                            break;
+                    }
+                }
+                catch
+                {
+                    continue;
+                }
+            }
+
+            return instance;
+        }
+    }
+}
